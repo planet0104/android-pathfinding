f9c7d4f8040935ebbf461747015b0df8a91b37ed
Jia Ye <planet0104@gmail.com>
2021-12-16 15:12:48 +0800

    1.0.3
    pathfinding依赖库改为bracket-pathfinding

--- pathfinding/src/main/jni/android-pathfinding/src/lib.rs
+++ pathfinding/src/main/jni/android-pathfinding/src/lib.rs
@@ -1,9 +1,10 @@
-use jni::objects::{JString, JObject};
-use jni::{objects::JClass, sys::jobjectArray};
-use jni::sys::{jint, jintArray};
-use jni::{JNIEnv, JavaVM};
 use anyhow::{anyhow, Result};
-use log::{info, error};
+use bracket_pathfinding::prelude::Point;
+use jni::objects::{JObject, JString};
+use jni::sys::{jint, jintArray};
+use jni::{objects::JClass, sys::jobjectArray};
+use jni::{JNIEnv, JavaVM};
+use log::{error, info};
 
 mod pathfinding;
 
@@ -14,75 +15,93 @@ pub extern "C" fn JNI_OnLoad(_jvm: JavaVM, _reserved: *mut std::ffi::c_void) -> 
     jni::sys::JNI_VERSION_1_6
 }
 
-fn jobject_array_to_grid<'a>(env: &'a JNIEnv, grid_obj: jobjectArray) -> Result<Vec<Vec<usize>>>{
-    let mut grid:Vec<Vec<usize>> = vec![];
+fn jobject_array_to_grid<'a>(env: &'a JNIEnv, grid_obj: jobjectArray) -> Result<Vec<Vec<u8>>> {
+    let mut grid: Vec<Vec<u8>> = vec![];
     let height = env.get_array_length(grid_obj)? as usize;
     //检查地图宽度是否相等
     let mut total_width = 0usize;
-    for i in 0..height{
+    for i in 0..height {
         let line = env.get_object_array_element(grid_obj, i as i32)?;
         let width = env.get_array_length(*line)?;
         total_width += width as usize;
         let mut row = vec![0; width as usize];
         env.get_int_array_region(*line, 0, &mut row)?;
-        grid.push(row.iter().map(|v| *v as usize).collect());
+        grid.push(row.iter().map(|v| *v as u8).collect());
     }
 
-    if total_width != height * grid[0].len(){
+    if total_width != height * grid[0].len() {
         Err(anyhow!("地图行长度不一致"))
-    }else{
+    } else {
         Ok(grid)
     }
 }
 
 #[no_mangle]
-pub extern fn Java_run_ccfish_android_pathfinding_PathFinding_loadMap<'a>(env: JNIEnv, _activity: JClass, grid_obj: jobjectArray){
-	let result = (||->Result<()> {
-        pathfinding::load_map(jobject_array_to_grid(&env, grid_obj)?)
-	})();
+pub extern "C" fn Java_run_ccfish_android_pathfinding_PathFinding_loadMap<'a>(
+    env: JNIEnv,
+    _activity: JClass,
+    grid_obj: jobjectArray,
+) {
+    let result = (|| -> Result<()> {
+        Ok(pathfinding::load_map(jobject_array_to_grid(
+            &env, grid_obj,
+        )?))
+    })();
 
-	if result.is_err(){
-		let err = result.err();
-		error!("{:?}", &err);
-		let _ = env.throw_new("java/lang/Exception", format!("{:?}", err));
-	}
+    if result.is_err() {
+        let err = result.err();
+        error!("{:?}", &err);
+        let _ = env.throw_new("java/lang/Exception", format!("{:?}", err));
+    }
 }
 
 #[no_mangle]
-pub extern fn Java_run_ccfish_android_pathfinding_PathFinding_loadMapForKey<'a>(env: JNIEnv, _activity: JClass, key: JString, grid_obj: jobjectArray){
-	let result = (||->Result<()> {
+pub extern "C" fn Java_run_ccfish_android_pathfinding_PathFinding_loadMapForKey<'a>(
+    env: JNIEnv,
+    _activity: JClass,
+    key: JString,
+    grid_obj: jobjectArray,
+) {
+    let result = (|| -> Result<()> {
         let grid = jobject_array_to_grid(&env, grid_obj)?;
         let key = env.get_string(key)?.to_str()?.to_string();
-        pathfinding::load_map_for_key(key, grid)
-	})();
+        Ok(pathfinding::load_map_for_key(key, grid))
+    })();
 
-	if result.is_err(){
-		let err = result.err();
-		error!("{:?}", &err);
-		let _ = env.throw_new("java/lang/Exception", format!("{:?}", err));
-	}
+    if result.is_err() {
+        let err = result.err();
+        error!("{:?}", &err);
+        let _ = env.throw_new("java/lang/Exception", format!("{:?}", err));
+    }
 }
 
 #[no_mangle]
-pub extern fn Java_run_ccfish_android_pathfinding_PathFinding_findPath<'a>(env: JNIEnv, _activity: JClass, x1: jint, y1: jint, x2: jint, y2:jint) -> jintArray{
-	match (||->Result<JObject> {
-        if x1<0 || y1<0 || x2<0 || y2<0{
+pub extern "C" fn Java_run_ccfish_android_pathfinding_PathFinding_findPath<'a>(
+    env: JNIEnv,
+    _activity: JClass,
+    x1: jint,
+    y1: jint,
+    x2: jint,
+    y2: jint,
+) -> jintArray {
+    match (|| -> Result<JObject> {
+        if x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 {
             return Err(anyhow!("坐标点不能小于0"));
         }
-        match pathfinding::find_path((x1 as usize, y1 as usize), (x2 as usize, y2 as usize))?{
+        match pathfinding::find_path(Point::new(x1, y1), Point::new(x2, y2))? {
             Some(path) => {
                 let mut val_arr = Vec::with_capacity(path.len() * 2);
-                for (x, y) in path{
-                    val_arr.push(x as i32);
-                    val_arr.push(y as i32);
+                for point in path {
+                    val_arr.push(point.x);
+                    val_arr.push(point.y);
                 }
                 let arr = env.new_int_array(val_arr.len() as i32)?;
                 env.set_int_array_region(arr, 0, &val_arr)?;
                 Ok(JObject::from(arr))
             }
-            None => Ok(JObject::null())
+            None => Ok(JObject::null()),
         }
-	})(){
+    })() {
         Ok(path) => path.into_inner(),
         Err(err) => {
             error!("{:?}", &err);
@@ -93,27 +112,35 @@ pub extern fn Java_run_ccfish_android_pathfinding_PathFinding_findPath<'a>(env: 
 }
 
 #[no_mangle]
-pub extern fn Java_run_ccfish_android_pathfinding_PathFinding_findPathInMap<'a>(env: JNIEnv, _activity: JClass, key: JString, x1: jint, y1: jint, x2: jint, y2:jint) -> jintArray{
-	match (||->Result<JObject> {
-        if x1<0 || y1<0 || x2<0 || y2<0{
+pub extern "C" fn Java_run_ccfish_android_pathfinding_PathFinding_findPathInMap<'a>(
+    env: JNIEnv,
+    _activity: JClass,
+    key: JString,
+    x1: jint,
+    y1: jint,
+    x2: jint,
+    y2: jint,
+) -> jintArray {
+    match (|| -> Result<JObject> {
+        if x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 {
             return Err(anyhow!("坐标点不能小于0"));
         }
         let key = env.get_string(key)?;
         let key = key.to_str()?;
-        match pathfinding::find_path_in_map(key, (x1 as usize, y1 as usize), (x2 as usize, y2 as usize))?{
+        match pathfinding::find_path_in_map(key, Point::new(x1, y1), Point::new(x2, y2))? {
             Some(path) => {
                 let mut val_arr = Vec::with_capacity(path.len() * 2);
-                for (x, y) in path{
-                    val_arr.push(x as i32);
-                    val_arr.push(y as i32);
+                for point in path {
+                    val_arr.push(point.x);
+                    val_arr.push(point.y);
                 }
                 let arr = env.new_int_array(val_arr.len() as i32)?;
                 env.set_int_array_region(arr, 0, &val_arr)?;
                 Ok(JObject::from(arr))
             }
-            None => Ok(JObject::null())
+            None => Ok(JObject::null()),
         }
-	})(){
+    })() {
         Ok(path) => path.into_inner(),
         Err(err) => {
             error!("{:?}", &err);
--- pathfinding/src/main/jni/android-pathfinding/src/pathfinding.rs
+++ pathfinding/src/main/jni/android-pathfinding/src/pathfinding.rs
@@ -1,114 +1,164 @@
-use std::{sync::RwLock, collections::HashMap};
-
-use hierarchical_pathfinding::prelude::*;
 use anyhow::{anyhow, Result};
+use bracket_pathfinding::prelude::{
+    a_star_search, Algorithm2D, BaseMap, DistanceAlg, Point, SmallVec,
+};
 use once_cell::sync::Lazy;
+use std::{collections::HashMap, sync::RwLock};
 
-pub type Point = (usize, usize);
+// Map
 
-const COST_MAP: [isize; 3] = [1, 10, -1];
-
-pub struct Pathfinder{
-    width: usize,
-    height: usize,
-    grid: Vec<Vec<usize>>,
-    pathfinding: PathCache<ManhattanNeighborhood>,
+pub struct Map {
+    pub width: usize,
+    pub height: usize,
+    pub tiles: Vec<u8>,
 }
 
-impl Pathfinder{
-    pub fn find(&self, start: Point, goal: Point) -> Result<Option<Vec<Point>>>{
-        if start.0 >= self.width || start.1 >= self.height{
-            return Err(anyhow!("point out of bounds. map size={}x{} start:{:?}, goal:{:?}", self.width, self.height, start, goal))
+impl Map {
+    pub fn new(grid: Vec<Vec<u8>>) -> Self {
+        let width = grid[0].len();
+        let height = grid.len();
+        let mut tiles = vec![];
+
+        for line in grid {
+            for t in line {
+                tiles.push(t);
+            }
         }
-        if goal.0 >= self.width || goal.1 >= self.height{
-            return Err(anyhow!("point out of bounds. map size={}x{} start:{:?}, goal:{:?}", self.width, self.height, start, goal))
+
+        Self {
+            width,
+            height,
+            tiles,
         }
-        match self.pathfinding.find_path(start, goal, move |(x, y)| COST_MAP[self.grid[y][x]]){
-            Some(path) => Ok(Some(path.collect())),
-            None => Ok(None)
+    }
+
+    pub fn a_star_search(&self, start: Point, end: Point) -> Option<Vec<Point>> {
+        let path = a_star_search(
+            self.point2d_to_index(start),
+            self.point2d_to_index(end),
+            self,
+        );
+        if path.success {
+            let steps = path.steps;
+            let mut points = Vec::with_capacity(steps.len());
+            for step in steps {
+                let pt = self.index_to_point2d(step);
+                points.push(pt);
+            }
+            Some(points)
+        } else {
+            None
         }
+    }
+
+    fn valid_exit(&self, loc: Point, delta: Point) -> Option<usize> {
+        let destination = loc + delta;
+        if self.in_bounds(destination) {
+            let idx = self.point2d_to_index(destination);
+            if self.tiles[idx] == 0 {
+                Some(idx)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl BaseMap for Map {
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx as usize] != 0
+    }
+
+    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
+        let mut exits = SmallVec::new();
+        let location = self.index_to_point2d(idx);
+
+        if let Some(idx) = self.valid_exit(location, Point::new(-1, 0)) {
+            exits.push((idx, 1.0))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(1, 0)) {
+            exits.push((idx, 1.0))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(0, -1)) {
+            exits.push((idx, 1.0))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(0, 1)) {
+            exits.push((idx, 1.0))
+        }
+
+        if let Some(idx) = self.valid_exit(location, Point::new(-1, -1)) {
+            exits.push((idx, 1.4))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(-1, 1)) {
+            exits.push((idx, 1.4))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(1, -1)) {
+            exits.push((idx, 1.4))
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(1, 1)) {
+            exits.push((idx, 1.4))
+        }
+
+        exits
+    }
+
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
+        DistanceAlg::Pythagoras.distance2d(self.index_to_point2d(idx1), self.index_to_point2d(idx2))
+    }
+}
+
+impl Algorithm2D for Map {
+    fn dimensions(&self) -> Point {
+        Point::new(self.width, self.height)
     }
 }
 
 /// 存放设置的单张地图
-static FINDER: Lazy<RwLock<Option<Pathfinder>>> = Lazy::new(|| { RwLock::new(None) });
+static MAP: Lazy<RwLock<Option<Map>>> = Lazy::new(|| RwLock::new(None));
 
 /// HashMap中存放多个地图
-static FINDERS: Lazy<RwLock<HashMap<String, Pathfinder>>> = Lazy::new(|| { RwLock::new(HashMap::new()) });
-
-
-pub fn new_path_finder(mut grid: Vec<Vec<usize>>) -> Result<Pathfinder>{
-
-    // 大于3改为2防止报错
-    grid.iter_mut().for_each(|row|{
-        row.iter_mut().for_each(|v|{
-            if *v != 0{
-                *v = 2;
-            }
-        });
-    });
-
-    let (height, width) = (grid.len(), grid[0].len());
-    
-    let grid_clone = grid.clone();
-
-    if width == 0 || height == 0 {
-        return Err(anyhow!("Error width={} height={}", width, height));
-    }
-
-    let pathfinding = PathCache::new(
-        (width, height),                           // the size of the Grid
-        move |(x, y)| COST_MAP[grid[y][x]],        // get the cost for walking over a Tile
-        ManhattanNeighborhood::new(width, height), // the Neighborhood
-        PathCacheConfig::with_chunk_size(3),       // config
-    );
-    Ok(Pathfinder{ width, height, grid: grid_clone, pathfinding })
-}
+static MAPS: Lazy<RwLock<HashMap<String, Map>>> = Lazy::new(|| RwLock::new(HashMap::new()));
 
 /// 加载单张地图
-pub fn load_map(grid: Vec<Vec<usize>>) -> Result<()>{
-    if let Ok(mut finder) = FINDER.write(){
-        finder.replace(new_path_finder(grid)?);
+pub fn load_map(grid: Vec<Vec<u8>>) {
+    if let Ok(mut finder) = MAP.write() {
+        finder.replace(Map::new(grid));
     }
-    Ok(())
 }
 
 /// 根据key加载地图
-pub fn load_map_for_key<K:Into<String>>(key: K, grid: Vec<Vec<usize>>) -> Result<()>{
-    if let Ok(mut finders) = FINDERS.write(){
-        finders.insert(key.into(), new_path_finder(grid)?);
+pub fn load_map_for_key<K: Into<String>>(key: K, grid: Vec<Vec<u8>>) {
+    if let Ok(mut finders) = MAPS.write() {
+        finders.insert(key.into(), Map::new(grid));
     }
-    Ok(())
 }
 
 /// 查找路径
-pub fn find_path(start: Point, goal: Point) -> Result<Option<Vec<Point>>>{
-    match FINDER.read(){
-        Ok(finder) => {
-            match finder.as_ref(){
-                Some(finder) => Ok(finder.find(start, goal)?),
-                None => Err(anyhow!("call load_map first!"))
-            }
-        }
-        Err(err) => Err(anyhow!("{:?}", err))
+pub fn find_path(start: Point, goal: Point) -> Result<Option<Vec<Point>>> {
+    match MAP.read() {
+        Ok(map) => match map.as_ref() {
+            Some(map) => Ok(map.a_star_search(start, goal)),
+            None => Err(anyhow!("call load_map first!")),
+        },
+        Err(err) => Err(anyhow!("{:?}", err)),
     }
 }
 
 /// 在指定地图中查找路径
-pub fn find_path_in_map(key: &str, start: Point, goal: Point) -> Result<Option<Vec<Point>>>{
-    match FINDERS.read(){
-        Ok(finders) => {
-            match finders.get(key){
-                Some(finder) => Ok(finder.find(start, goal)?),
-                None => Err(anyhow!("call load_map_for_key first!"))
-            }
-        }
-        Err(err) => Err(anyhow!("{:?}", err))
+pub fn find_path_in_map(key: &str, start: Point, goal: Point) -> Result<Option<Vec<Point>>> {
+    match MAPS.read() {
+        Ok(maps) => match maps.get(key) {
+            Some(map) => Ok(map.a_star_search(start, goal)),
+            None => Err(anyhow!("call load_map_for_key first!")),
+        },
+        Err(err) => Err(anyhow!("{:?}", err)),
     }
 }
 
 #[test]
-fn test_find() -> Result<()>{
+fn test_find() -> Result<()> {
     let grid = vec![
         vec![0, 3, 0, 0, 0],
         vec![0, 3, 3, 3, 3],
@@ -117,16 +167,25 @@ fn test_find() -> Result<()>{
         vec![0, 0, 0, 3, 0],
     ];
 
-    load_map(grid)?;
+    load_map(grid);
 
-    let points = find_path((0, 0), (4, 4))?.unwrap();
+    let points = find_path(Point::new(0, 0), Point::new(4, 4))?.unwrap();
 
     println!("{:?}", points);
-    
+
     assert_eq!(
-         points,
-         vec![(0, 1),  (0, 2),  (0, 3),  (0, 4),  (1, 4),  (2, 4),
-             (2, 3),  (2, 2),  (3, 2),  (4, 2),  (4, 3),  (4, 4)],
+        points,
+        vec![
+            Point::new(0, 0),
+            Point::new(0, 1),
+            Point::new(0, 2),
+            Point::new(0, 3),
+            Point::new(1, 4),
+            Point::new(2, 3),
+            Point::new(3, 2),
+            Point::new(4, 3),
+            Point::new(4, 4)
+        ],
     );
 
     let grid = vec![
@@ -136,10 +195,10 @@ fn test_find() -> Result<()>{
         vec![0, 3, 0, 3, 0],
         vec![0, 0, 0, 3, 0],
     ];
-    
-    load_map_for_key("map1", grid)?;
-    
-    let points = find_path_in_map("map1", (0, 0), (0, 0))?;
+
+    load_map_for_key("map1", grid);
+
+    let points = find_path_in_map("map1", Point::new(0, 0), Point::new(0, 0))?;
 
     println!("{:?}", points);
 
